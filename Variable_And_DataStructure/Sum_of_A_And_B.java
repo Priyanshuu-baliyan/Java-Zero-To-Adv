eb2c71766a8ff4aa30385148e78bac9b804a8bb5
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-17 01:00:05 +0530

    sum of two number

---
+++ Variable_And_DataStructure/Sum_of_A_And_B.java
@@ -0,0 +1,11 @@
+package Variable_And_DataStructure;
+
+public class Sum_of_A_And_B {
+  public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+    int sum = a + b;
+    System.out.println(sum);
+  }
+}
+ 
