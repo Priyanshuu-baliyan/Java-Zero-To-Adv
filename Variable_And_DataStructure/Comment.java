3c4a7f290b0e796bb1dccdaf41378108eb650395
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-17 01:18:09 +0530

    comment

---
+++ Variable_And_DataStructure/Comment.java
@@ -0,0 +1,12 @@
+package Variable_And_DataStructure;
+
+public class Comment {
+  // code to calc Sum
+  /*public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+    int sum = a + b;
+    System.out.println(sum);
+  }
+  */
+}
