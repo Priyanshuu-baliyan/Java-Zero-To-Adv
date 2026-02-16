63300de826acf18e0dd0ae302a6486a955bc52a9
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-17 00:13:56 +0530

    DataType Overview

---
+++ Variable_And_DataStructure/DataType.java
@@ -0,0 +1,19 @@
+package Variable_And_DataStructure;
+public class DataType {
+  public static void main(String[] args) {
+    byte b = 8;
+    System.out.println(b);
+    char ch = 'a';
+    boolean var = false;
+    float price = 10.5f;
+    int number = 25;
+    //long 
+    //double
+    short n = 240;  
+    System.out.println(ch); 
+    System.out.println(var); 
+    System.out.println(price); 
+    System.out.println(number); 
+    System.out.println(n);  
+  }
+}
