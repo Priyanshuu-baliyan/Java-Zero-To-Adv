02cf7670c67141d8cbc6f98da37c332ab34c7921
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-16 23:50:19 +0530

    Variables

---
+++ Variable_And_DataStructure/Variables.java
@@ -0,0 +1,13 @@
+package Variable_And_DataStructure;
+
+public class Variables {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    System.out.println(a);
+    System.out.println(b);
+
+    String name = "Batman";
+    System.out.println(name);
+  }
+}
