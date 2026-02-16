cb513daa388ea24c96ef8812af8729247a7577bd
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-16 23:31:26 +0530

    Output in Java

--- Variable_And_DataStructure/JavaBasics.java
+++ Variable_And_DataStructure/JavaBasics.java
@@ -2,6 +2,8 @@ package Variable_And_DataStructure;
 
 public class JavaBasics {
   public static void main (String[] args){
+    System.out.println("1,2,4"); // println : for spacing the line
+    System.out.print("Hello world\n"); // \n: for spacing the line
     System.out.print("This is a output");
   }
 }
