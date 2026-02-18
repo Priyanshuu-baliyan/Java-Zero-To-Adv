bcadd79a90ff6c597c82a6a1ce74635390c63466
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-02-18 10:20:36 +0530

    sum of two number

--- Variable_And_DataStructure/Sum_Through_UserInput.java
+++ Variable_And_DataStructure/Sum_Through_UserInput.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Sum_Through_UserInput {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Enter your num1: ");
     int a = sc.nextInt();
+    System.out.print("Enter your num2: ");
     int b = sc.nextInt();
     int sum = a + b;
     System.out.println("Sum of a & b is : " + sum);
