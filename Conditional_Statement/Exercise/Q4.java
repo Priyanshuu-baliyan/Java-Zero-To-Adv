096dfc00a0e2b475627be71221547798774841fe
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-03-06 22:25:28 +0530

    Exercise

--- Conditional_Statement/Exercise/Q4.java
+++ Conditional_Statement/Exercise/Q4.java
@@ -2,7 +2,7 @@ package Conditional_Statement.Exercise;
 
 public class Q4 {
   public static void main(String[] args) {
-    int a = 63, b = 36;
+    int a = 63, b = 64;
     boolean x = (a < b ) ? true : false;
     int y= (a > b ) ? a : b;
     System.out.println("Value of X is : " + x);
