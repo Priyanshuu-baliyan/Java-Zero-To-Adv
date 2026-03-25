2155405d43afe2d780fee30b6180420d4a7b34ae
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-03-25 20:41:52 +0530

    Loops Practice

--- Loops/Exercise/Q3.java
+++ Loops/Exercise/Q3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Q3 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the Factorial number: ");
+    System.out.print("Enter the any positive integer number: ");
     int n = sc.nextInt();
     int fact = 1;
     if(n == 0 || n == 1){
