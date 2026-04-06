3f6151935d680d47a62699f992d66adac3b00ddb
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-04-06 23:57:41 +0530

    Function and Methods

---
+++ Function_And_Methods/BasicFn.java
@@ -0,0 +1,16 @@
+package Function_And_Methods;
+
+
+public class BasicFn{
+
+  public static void printHello(){
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+  public static void main(String[] args) {
+    printHello();    
+    printHello();    
+
+  }
+}
