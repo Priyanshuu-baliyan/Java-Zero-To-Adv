df0e292554457aa10341451431c5e37aa05b82da
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-07-27 20:01:21 +0530

    Getter & Setters

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -1,39 +1,30 @@
 package OOPS;
-import java.util.*;
-public class OOPS {
+
+public class OOPS{
   public static void main(String[] args) {
     Pen p1 = new Pen();
-    p1.color = "Blue";
-    p1.tip = 5;
-    System.out.println(p1.color);
-    System.out.println(p1.tip);
-    p1.color = "Yellow";
-    System.out.println(p1.color);
+    p1.setColor("Blue");
+    p1.setTip(5);
 
-    Bank myAcc = new Bank();
-    myAcc.Username = "Vernik Kumar";
-    myAcc.setPassword("abcdefghi");
-    System.out.println(myAcc.Username);
-    // System.out.println(myAcc.password);
-  }
-}
-
-class Bank{
-  public String Username;
-  private String password;
-  void setPassword(String pwd){
-    password = pwd;
+    System.out.println(p1.getColor());
+    System.out.println(p1.getTip());
   }
 }
 class Pen{
-  String color;
-  int tip;
+  private String color;
+  private int tip;
 
   void setColor(String newColor){
-    color = newColor;
+    this.color = newColor;
+  }
+  void setTip(int tip){
+    this.tip = tip;
   }
 
-  void setTip(int newTip){
-    tip = newTip;
+  String getColor(){
+    return this.color;
+  }
+  int getTip(){
+    return this.tip;
   }
 }
