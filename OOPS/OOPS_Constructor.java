e748fa0c1ffbd7758bd2fcb9057336ae1abce6a0
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-07-28 22:43:10 +0530

    Type of Constructor

--- OOPS/OOPS_Constructor.java
+++ OOPS/OOPS_Constructor.java
@@ -2,16 +2,24 @@ package OOPS;
 
 public class OOPS_Constructor {
   public static void main(String[] args) {
-    constructor c1 = new constructor("Priyanshu");
-    System.out.println(c1.name);
+    constructor c1 = new constructor();
+    constructor c2 = new constructor("Priyanshu");
+    constructor c3 = new constructor(5123);
+
+    // System.out.println(c1.name);
   }
 }
 class constructor{
   String name;
   int roll;
 
+  constructor(){
+    System.out.println("Constructors is called...");
+  }
   constructor(String name){
     this.name = name;
-    // System.out.println("Constructors is called...");
+  }
+  constructor(int roll){
+    this.roll = roll;
   }
 }
