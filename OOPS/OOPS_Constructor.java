d0ab4ba0b4499ac485e50882baf984b0e2d76c08
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-07-28 23:22:45 +0530

    Copy Constructor

--- OOPS/OOPS_Constructor.java
+++ OOPS/OOPS_Constructor.java
@@ -2,24 +2,47 @@ package OOPS;
 
 public class OOPS_Constructor {
   public static void main(String[] args) {
-    constructor c1 = new constructor();
-    constructor c2 = new constructor("Priyanshu");
-    constructor c3 = new constructor(5123);
+    Constructor c1 = new Constructor();
+    c1.name = "Priyanshu";
+    c1.roll = 620;
+    c1.password = "asdfgh";
+    c1.marks[0] = 95;
+    c1.marks[1] = 97;
+    c1.marks[2] = 100;
 
-    // System.out.println(c1.name);
+    Constructor c2 = new Constructor(c1);
+    c2.password = "xyzqw";
+    c2.marks[1] = 98;
+    for(int i = 0; i < 3; i++){
+      System.out.println(c2.marks[i]);
+    }
   }
 }
-class constructor{
+class Constructor{
   String name;
   int roll;
+  String password;
+  int marks[];
 
-  constructor(){
+  //copy constructor
+  Constructor(Constructor c1){
+    this.name = c1.name;
+    this.roll = c1.roll;
+    marks = new int[3];
+    this.marks = c1.marks;
+
+  }
+
+  Constructor(){
+    marks = new int[3];
     System.out.println("Constructors is called...");
   }
-  constructor(String name){
+  Constructor(String name){
+    marks = new int[3];
     this.name = name;
   }
-  constructor(int roll){
+  Constructor(int roll){
+    marks = new int[3];
     this.roll = roll;
   }
 }
