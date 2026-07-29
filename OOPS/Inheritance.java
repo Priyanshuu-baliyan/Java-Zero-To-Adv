f03ea9ceac3efc5669ddf2a895a0e4de8623f540
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-07-29 23:59:33 +0530

    Inheritance

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class Inheritance{
+  public static void main(String[] args) {
+    
+  }
+}
+
+//base class
+class Animal{
+  String color;
+  void eat(){
+    System.out.println("Eats..!");
+  }
+}
