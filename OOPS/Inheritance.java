ea67de592bdbb7b5ebf2b1b0161764d784f22055
Priyanshuu-baliyan <priyanshubaliyan494@gmail.com>
2026-07-30 00:32:24 +0530

    Inheritance

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -2,7 +2,8 @@ package OOPS;
 
 public class Inheritance{
   public static void main(String[] args) {
-    
+    Tiger t1 = new Tiger();
+    t1.eat(); 
   }
 }
 
@@ -12,4 +13,15 @@ class Animal{
   void eat(){
     System.out.println("Eats..!");
   }
+  void breathe(){
+    System.out.println("Breathes");
+  }
+}
+
+//derived class
+class Tiger extends Animal{
+  int leg;
+  void endurance(){
+    System.out.println("He is so fit");
+  } 
 }
